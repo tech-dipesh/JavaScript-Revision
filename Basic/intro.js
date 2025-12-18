abbff7d4868e3f0e9b6deae364c252ef8417f2c3
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-12-18 22:13:23 +0530

    Add the first commit of the JavaScirpt REvision Part

---
+++ Basic/intro.js
@@ -0,0 +1,20 @@
+console.log("Hello wolrd");
+
+
+for(let i=10;i<2882;i++){
+  console.log(i);
+}
+
+
+
+let names="Dipesh sharma"
+console.log(names);
+
+
+
+let n="98322";
+const nn="823";
+var nnn="293";
+console.log(n, nn, nnn);
+
+
