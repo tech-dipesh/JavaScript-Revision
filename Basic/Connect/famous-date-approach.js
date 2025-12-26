6b50ea13345adb476978d845059dff16da44f237
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-12-26 18:25:02 +0530

    Add the Famous Date approach that we use also add teh some basic js connect method

---
+++ Basic/Connect/famous-date-approach.js
@@ -0,0 +1,11 @@
+console.log(new Date().toUTCString());
+console.log(new Date());
+
+console.log(new Date().getDate());
+console.log(new Date().getTime());
+console.log(new Date().getTimezoneOffset());
+
+
+  console.log(new Date().toLocaleTimeString());
+
+console.log(new Date().getFullYear());
---
+++ Basic/Connect/script.js
@@ -0,0 +1,8 @@
+/** 
+ * ! This is the warning comment
+ * ? jadksj
+*/
+  function change() {
+      console.log("helore");
+    document.getElementById('name').innerHTML="Test naem"
+  }
