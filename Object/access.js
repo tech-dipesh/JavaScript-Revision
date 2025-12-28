9c896cc838f06c0a6acda47e1962456ba1568bcd
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-12-28 22:21:36 +0530

    Add the object access method and intro of the object

---
+++ Object/access.js
@@ -0,0 +1,11 @@
+let University={
+  studentList: 283,
+  topperName: "Anish giri",
+  bestTeacher: "Rohan Sharma",
+  areaOfUniversity: 19832,
+  isSafe: true
+}
+
+console.log(University.topperName);
+console.log(University['topperName']);
+console.log(University['bestTeacher']);
---
+++ Object/intro.js
@@ -0,0 +1,11 @@
+let list={
+  name: "bikas",
+  class: 8,
+  roll_id:38
+}
+
+// console.log(list.name);
+delete list.name
+console.log(list);
+
+console.log(typeof(list));
