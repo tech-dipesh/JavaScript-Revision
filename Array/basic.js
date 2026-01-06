2966d2c4cc1d4768652cc594a51a380d8c0c0ba0
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2026-01-06 22:54:55 +0530

    add teh basic aray and the ntsted array

---
+++ Array/basic.js
@@ -0,0 +1,13 @@
+let a=[2, 3, 4, 5]
+console.log(a);
+
+
+let b=[2, "2", "3", "4", "5"]
+console.log(b);
+let sum=0;
+for(let i=0;i<b.length;i++){
+  sum+=b[i];
+  console.log(b[i]);
+}
+
+console.log(sum);
---
+++ Array/nested-array.js
@@ -0,0 +1,11 @@
+let Array=[238, false,
+  ["Nested Array", 93,
+     ["Another Nested"]
+    ]
+]
+
+console.log(Array);
+
+console.log(Array[2]);
+console.log(Array[2[1]]);
+// console.log(Array[5]);
