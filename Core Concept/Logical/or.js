498f1fb2434691d80ebea47f3545bdf3e769561d
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2026-01-28 23:03:19 +0530

    add all the 3 logical operatoin

---
+++ Core Concept/Logical/or.js
@@ -0,0 +1,21 @@
+let isPerson=90
+let Value=58
+
+if(isPerson==90 || Value==58){
+  console.log("Both are true");
+}
+else{
+  console.log("Eitheer false or both false");
+}
+
+
+
+let nu=80
+let su=28
+
+if(nu>=90 || su>=20){
+  console.log("Both are true");
+}
+else{
+  console.log("Eitheer false or both false");
+}
